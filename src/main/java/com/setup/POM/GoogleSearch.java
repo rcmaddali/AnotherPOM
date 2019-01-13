a62017eaeaf99b26d83a8d14b48ea6031f746382
rcmaddali <ravimaddali@ravis-MacBook-Air.local>
2019-01-12 22:57:25 -0500

    Issues with chromedriver resolved

---
+++ src/main/java/com/setup/POM/GoogleSearch.java
@@ -0,0 +1,30 @@
+package com.setup.POM;
+
+/**
+ * This class contain all element locators
+ */
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class GoogleSearch {
+	WebDriver driver;
+	WebElement element;
+	By searchBox = By.name("q");
+	By searchButton = By.name("btnK");
+
+	public GoogleSearch(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	public void typeSearch() {
+
+		driver.findElement(searchBox).sendKeys("Made It after resolving issues");
+	}
+
+	public void clickSearch() {
+		
+		driver.findElement(searchButton).click();
+	}
+}
---
+++ src/test/java/com/setup/POM/GoogleResults.java
@@ -0,0 +1,38 @@
+package com.setup.POM;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * This class contains the test methods
+ */
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.setup.POM.GoogleSearch;
+
+public class GoogleResults {
+
+	static WebDriver driver = null;
+
+	@BeforeClass
+	public static void checkproperty() {
+		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver");
+		driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+
+	}
+
+	@Test
+	public void validateResults()  {
+
+		driver.get("http://google.com");
+		GoogleSearch search = new GoogleSearch(driver);
+		search.typeSearch();
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS) ;
+		search.clickSearch();
+		driver.quit();
+	}
+
+}
